f0d7c218dc19c23b51918d722b5b9b669062b1da
Anthony <anthony.guiock@gmail.com>
2020-04-23 18:10:09 +0200

    Add virus choice menu

---
+++ Pandor/Assets/Scripts/GameplayEngine/Virus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string virusName;
+    [Range(0,100)]
+    public float tauxPropagation;
+    [Range(0,100)]
+    public float tauxMortalite;
+    static public Virus actualVirus;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void SetActualVirus(Virus virus)
+    {
+        Debug.Log("Le virus selectionné était : " + actualVirus);
+        actualVirus = virus;
+        Debug.Log(", il a été changé en : " + actualVirus);
+    }
+}
