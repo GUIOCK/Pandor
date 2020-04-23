2ca3fd7754e63d75594af235ba7c06905c3bf26e
Yanis Verbeke <yanis88357@gmail.com>
2020-04-24 00:51:37 +0200

    Calcul de Risque

--- Pandor/Assets/Scripts/Gameplay/CalculInfection.cs
+++ Pandor/Assets/Scripts/Gameplay/CalculInfection.cs
@@ -2,17 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CalculInfection : MonoBehaviour
 {
+    InfectedScript infectedScript;
+    int maxIncrementValue = 10;
+
+    int tauxPortMasque = 0;         // Var paramètres foyer à récup
+    int tauxUtilGel = 0;            //
+    int tauxConfinement = 0;        //
+
+    private int risqueAvecMasque = 16;
+    private int risqueAvecGel = 0;
+    private int risqueAvecConfinement = 0;
+
+    private int risqueSansMasque = 95;
+    private int risqueSansGel = 0;
+    private int risqueSansConfinement = 0;
+
+    private int risqueTotal = 0;        // Pourcentage
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        infectedScript = GetComponentInChildren<InfectedScript>();
+        Invoke("Increment", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Increment()
+    {
+        Debug.Log("pouet");
+
+        risqueTotal = ((risqueAvecMasque * (tauxPortMasque / 100)) + (risqueSansMasque * (1 - (tauxPortMasque / 100))) + (risqueAvecGel * (tauxUtilGel / 100)) + (risqueSansGel * (1 - (tauxUtilGel / 100))) + (risqueAvecConfinement * (tauxConfinement / 100)) + (risqueSansConfinement * (1 - (tauxConfinement / 100)))) / 3;
+        Debug.Log("Risque total : " + risqueTotal + "%");
+        maxIncrementValue = risqueTotal; // La vitesse d'augmentation maximale correspond au risque d'atrapper le virus (ex : si 100% de risque, 100% du foyer va finir contaminé très rapidement)
+
+        infectedScript.infectionRate += Random.Range(0, maxIncrementValue);
+        if (infectedScript.infectionRate > 100)
+        {
+            infectedScript.infectionRate = 100;
+        }
+        Invoke("Increment", 2f);
     }
 }
--- Pandor/Assets/Scripts/Gameplay/InfectedScript.cs
+++ Pandor/Assets/Scripts/Gameplay/InfectedScript.cs
@@ -6,7 +6,7 @@ public class InfectedScript : MonoBehaviour
 {
     [SerializeField]
     [Range(0, 100)]
-    int infectionRate = 0;
+    public int infectionRate = 0;
 
     // Start is called before the first frame update
     void Start()
