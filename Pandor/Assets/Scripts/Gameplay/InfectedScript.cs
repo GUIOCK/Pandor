898c077671ff4d9174dc4fbf64cef4796cfe48f6
PulbyA <antonin.pulby@edu.itescia.fr>
2020-04-24 05:35:33 +0200

    Side menu
    Side menu in game permettant lecture et modification des paramètre d'un foyer

--- Pandor/Assets/Scripts/Gameplay/InfectedScript.cs
+++ Pandor/Assets/Scripts/Gameplay/InfectedScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,25 @@ public class InfectedScript : MonoBehaviour
     [Range(0, 100)]
     int infectionRate = 0;
 
+    [SerializeField]
+    [Range(0, 100)]
+    private int quarantineRespect = 0;
+
+    [SerializeField]
+    [Range(0, 100)]
+    private int masks = 0;
+
+    [SerializeField]
+    [Range(0, 100)]
+    private int washingHands = 0;
+
+    [SerializeField] private GameObject sideMenu;
+    [SerializeField] private String nameFoyer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,5 +39,73 @@ public class InfectedScript : MonoBehaviour
         color.r = (float)infectionRate / 100;
         color.g = (100 - (float)infectionRate) / 100;
         renderer.material.color = color;
+    }
+    
+    void OnMouseDown()
+    {
+        DisplaySideMenu();
+    }
+
+    public void DisplaySideMenu()
+    {
+        sideMenu.SetActive(!sideMenu.active);
+        if (sideMenu.active)
+        {
+            this.tag = "SelectedFoyer";
+        }
+        else
+        {
+            this.tag = "FreeFoyer";
+        }
+    }
+
+    public int getInfectionRate()
+    {
+        return this.infectionRate;
+    }
+
+    public void setInfectionRate ( int infectionRate)
+    {
+        this.infectionRate = infectionRate;
+    }
+
+    public int getQuarantineRespect()
+    {
+        return quarantineRespect;
+    }
+
+    public void setQuarantineRespect(int quarantineRespect)
+    {
+        this.quarantineRespect = quarantineRespect;
+    }
+
+    public int getMasks()
+    {
+        return masks;
+    }
+
+    public void setMasks(int masks)
+    {
+        this.masks = masks;
+    }
+
+    public int getWashingHands()
+    {
+        return washingHands;
+    }
+
+    public void setWashingHands(int washingHands)
+    {
+        this.washingHands = washingHands;
+    }
+
+    public string getNameFoyer()
+    {
+        return nameFoyer;
+    }
+
+    public void setNameFoyer(string nameFoyer)
+    {
+        this.nameFoyer = nameFoyer;
     }
 }
---
+++ Pandor/Assets/Scripts/Gameplay/SideMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideMenu : MonoBehaviour
+{
+    private InfectedScript foyer;
+
+    private int washingHands;
+    private int quarantineRespect;
+    private int masks;
+    private string nameFoyer;
+
+    private Text tauxQuarantineRespect;
+    private Text tauxWashingHands;
+    private Text tauxMasks;
+    private Text libelleNameFoyer;
+    private Slider sliderQuarantineRespect;
+    private Slider sliderWashingHands;
+    private Slider sliderMasks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("SelectedFoyer") != null || GameObject.FindGameObjectsWithTag("SelectedFoyer").Length != 0)
+        {
+            foyer = (InfectedScript)GameObject.FindGameObjectsWithTag("SelectedFoyer")[0].GetComponent("InfectedScript");
+            washingHands = foyer.getWashingHands();
+            quarantineRespect = foyer.getQuarantineRespect();
+            masks = foyer.getMasks();
+            nameFoyer = foyer.getNameFoyer();
+
+            libelleNameFoyer = this.transform.GetChild(0).GetComponentInChildren<Text>();
+            tauxQuarantineRespect = this.transform.GetChild(1).GetComponentInChildren<Text>();
+            tauxMasks = this.transform.GetChild(2).GetComponentInChildren<Text>();
+            tauxWashingHands = this.transform.GetChild(3).GetComponentInChildren<Text>();
+            sliderQuarantineRespect = this.transform.GetChild(4).GetComponentInChildren<Slider>();
+            sliderMasks = this.transform.GetChild(5).GetComponentInChildren<Slider>();
+            sliderWashingHands = this.transform.GetChild(6).GetComponentInChildren<Slider>();
+
+            libelleNameFoyer.text = nameFoyer;
+            tauxQuarantineRespect.text = quarantineRespect.ToString();
+            tauxWashingHands.text = washingHands.ToString();
+            tauxMasks.text = masks.ToString();
+
+            sliderQuarantineRespect.value = quarantineRespect / 100f;
+            sliderWashingHands.value = washingHands / 100f;
+            sliderMasks.value = masks / 100f;
+        }
+    }
+
+}
