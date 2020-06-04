9a23cc70759881588d45e81347fd9b62f2df8d22
Unknown <thibaud.roussel@edu.itescia.fr>
2020-06-04 10:34:07 +0200

    Pathfinding
    Add pathfinding map to test him

---
+++ Pandor/Assets/Scripts/Control/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class CharacterController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public bool isArrived;
+    public float tagetX;
+
+    [SerializeField] private GameObject market;
+
+    private Vector3 target;
+    private Vector3 house;
+    private float timeStaying = 5f;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        target = new Vector3(tagetX, transform.position.y, -5.4f);
+        //house = new Vector3(0f, 0f, 0f);
+        house = transform.position;
+        
+    }
+
+
+    void Update()
+    {
+        Debug.Log("ma pos : " + transform.position);
+		if (Input.GetMouseButtonDown(0))
+		{
+            agent.SetDestination(target);
+        }
+        else if (isArrived)
+        {
+            isArrived = false;
+            InvokeRepeating("DestinationToHouse", timeStaying, 10f);
+            
+        }
+    }
+
+
+    public void DestinationToHouse()
+    {
+        agent.SetDestination(house);
+        CancelInvoke();
+
+
+    }
+
+
+}
---
+++ Pandor/Assets/Scripts/Environement/GroupingAreaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupingAreaController : MonoBehaviour
+{
+    CharacterController ch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ch = new CharacterController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            CharacterController ch = other.gameObject.GetComponent<CharacterController>();
+            ch.isArrived = true;
+            Debug.Log("hELLO");
+
+        }
+    }
+}
