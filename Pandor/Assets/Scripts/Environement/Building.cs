fb5eaa015d42e753363018a41fca29785f0f9f81
Unknown <thibaud.roussel@edu.itescia.fr>
2020-06-15 18:25:29 +0200

    Upgrade Pathfinding, autogeneration pnj
    No collision between  Pnj, they can return to our house after x time and

---
+++ Pandor/Assets/Scripts/Environement/Building.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private GameObject[] allBuilding;
+
+    private GameObject[] house;
+
+    public GameObject[] getBuildings
+    {
+        get { return allBuilding; }
+    }
+
+    public GameObject[] arrayHouse
+    {
+        get { return house; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        house = GameObject.FindGameObjectsWithTag("House");
+    }
+
+
+}
